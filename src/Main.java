06838a6075b4b3e77d62498cff8ae8f7fdbcfbc4
Rafael <ribeiro.rafajunior@gmail.com>
2024-06-05 16:55:46 -0300

    Projeto inicial

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import Principal.Imobiliaria;
+
+public class Main {
+    public static void main(String[] args) {
+        Imobiliaria imobiliaria = new Imobiliaria();
+        imobiliaria.exibeMenu();
+    }
+}
---
+++ src/Models/Endereco.java
@@ -0,0 +1,37 @@
+package Models;
+
+public class Endereco {
+    private String cidade;
+    private String bairro;
+
+    public Endereco() {
+    }
+
+    public Endereco(String cidade, String bairro) {
+        this.cidade = cidade;
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "\nCidade = " + cidade +
+                "\nBairro = " + bairro + "\n";
+    }
+}
